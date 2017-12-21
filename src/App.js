1260f28a2e9b2c7c258a074e2a9c227f7aa6f5e1
Arjun Nemani <nemaniarjun@gmail.com>
2017-12-21 22:31:56 +0530

    better positioning of footer

--- src/App.js
+++ src/App.js
@@ -7,14 +7,32 @@ import "./GithubRibbon.css";
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <center className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h3 className="App-title">React TODO List</h3>
-        </center>
-        <div className="list-container">
-          <TodoListContainer />
+      <div style={{ position: "relative" }}>
+        <div className="App container">
+          <center className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h3 className="App-title">React TODO List</h3>
+          </center>
+          <div className="list-container">
+            <TodoListContainer />
+          </div>
         </div>
+        <center id="footer">
+          <a
+            href="http://nemaniarjun.github.io/"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            Made with{" "}
+            <svg className="icon icon-coffee">
+              <use xlinkHref="#icon-coffee" />
+            </svg>{" "}
+            &{" "}
+            <svg className="icon icon-heart">
+              <use xlinkHref="#icon-heart" />
+            </svg>{" "}
+            Arjun Nemani
+          </a>
+        </center>
       </div>
     );
   }
