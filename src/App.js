a238c450a06b60ded907f2c973d77cc392ad1c16
Arjun Nemani <nemaniarjun@gmail.com>
2017-12-21 22:37:05 +0530

    by missing

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import "./App.css";
 import "./GithubRibbon.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div style={{ position: "relative" }}>
-        <div className="App container">
-          <center className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h3 className="App-title">React TODO List</h3>
-          </center>
-          <div className="list-container">
-            <TodoListContainer />
-          </div>
-        </div>
-        <center id="footer">
-          <a
-            href="http://nemaniarjun.github.io/"
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            Made with{" "}
-            <svg className="icon icon-coffee">
-              <use xlinkHref="#icon-coffee" />
-            </svg>{" "}
-            &{" "}
-            <svg className="icon icon-heart">
-              <use xlinkHref="#icon-heart" />
-            </svg>{" "}
-            Arjun Nemani
-          </a>
-        </center>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{ position: "relative" }}>
+				<div className="App container">
+					<center className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h3 className="App-title">React TODO List</h3>
+					</center>
+					<div className="list-container">
+						<TodoListContainer />
+					</div>
+				</div>
+				<center id="footer">
+					<a
+						href="http://nemaniarjun.github.io/"
+						style={{ color: "inherit", textDecoration: "none" }}
+					>
+						Made with{" "}
+						<svg className="icon icon-coffee">
+							<use xlinkHref="#icon-coffee" />
+						</svg>{" "}
+						&{" "}
+						<svg className="icon icon-heart">
+							<use xlinkHref="#icon-heart" />
+						</svg>{" "}
+						by Arjun Nemani
+					</a>
+				</center>
+			</div>
+		);
+	}
 }
 
 export default App;
